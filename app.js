3a790588ceb365f8ed31b15a2332422247200e40
Jake Witcher <jjwitcher09@gmail.com>
2018-08-13 02:12:38 +0000

    add environment variables

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index");
     
 
-//mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
-
-mongoose.connect("mongodb://jameswitcher:enter129rest@ds119732.mlab.com:19732/yelpcamp_witcherjake", {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+//mongoose.connect("mongodb://jameswitcher:enter129rest@ds119732.mlab.com:19732/yelpcamp_witcherjake", {useNewUrlParser: true});
+//"mongodb://localhost:27017/yelp_camp"
+console.log;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
